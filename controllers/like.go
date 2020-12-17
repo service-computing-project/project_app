12142731954e93c43701f695c000201fcd76730e
lijia73 <gaillee666@outlook.com>
2020-12-17 12:47:10 +0800

    增加十六进制判断

---
+++ controllers/like.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/sessions"
+	"github.com/globalsign/mgo/bson"
+	"github.com/yilin0041/project_app/models"
+)
+
+type LikeController struct {
+	Ctx     iris.Context
+	Model   models.UserDB
+	Session *sessions.Session
+}
+
+// LikeRes 用户点赞数据返回值
+type LikeRes struct {
+	State string
+	Data  []string
+}
+
+// GetBy Get /like/{contentID} 获取用户点赞列表
+func (c *LikeController) GetBy(id string) (res LikeRes){
+	if !bson.IsObjectIdHex(id) {
+		res.State = StatusBadReq
+		return
+	}
+	var err error
+	res.Data,err = c.Model.GetUserListByContentID(id)
+	if err != nil {
+		res.State = err.Error()
+	}
+	res.State = models.StatusSuccess
+	return
+}
+
+//​ PostBy POST /like/{contentID} 对某个内容点赞
+func (c *LikeController) PostBy(id string) (res models.CommonRes){
+	if c.Session.Get("id") == nil {
+		res.State = models.StatusNotLogin
+		return
+	}
+	if !bson.IsObjectIdHex(id) {
+		res.State = StatusBadReq
+		return
+	}
+	err := c.Model.LikeByID(id,c.Session.Get("id"))
+	if err != nil {
+		res.State = err.Error()
+	}
+	res.State = models.StatusSuccess
+	return
+}
+
+//​ PatchBy PATCH /like/{contentID} 取消用户对某个内容的点赞
+func (c *LikeController) PatchBy(id string) (res models.CommonRes){
+	if c.Session.Get("id") == nil {
+		res.State = models.StatusNotLogin
+		return
+	}
+	if !bson.IsObjectIdHex(id) {
+		res.State = StatusBadReq
+		return
+	}
+	err := c.Model.CancelLikeByID(id,c.Session.Get("id"))
+	if err != nil {
+		res.State = err.Error()
+	}
+	res.State = models.StatusSuccess
+	return	
+}
+
+
+
+
+
