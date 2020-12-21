737e104b593e5eb79fe2bac84157c7069d730686
sunylin <1098951701@qq.com>
2020-12-21 18:06:12 +0800

    update options

--- controllers/like.go
+++ controllers/like.go
@@ -35,7 +35,7 @@ func (c *LikeController) GetBy(id string) (res LikeRes) {
 }
 
 //Options Options /like/{contentID} 对某个内容点赞
-func (c *LikeController) Options() {
+func (c *LikeController) OptionsBy(id string) {
 	return
 }
 
