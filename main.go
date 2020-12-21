85521f6f1662c535ab0df4d53eb6c3b8d76ed28a
lijia73 <gaillee666@outlook.com>
2020-12-21 19:46:12 +0800

    add Access-Control-Allow-Origin

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func main() {
 	})
 	app.Use(sess.Handler())
 	crs := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"}, //允许通过的主机名称
+		//AllowedOrigins:   []string{"*"}, //允许通过的主机名称
 		AllowCredentials: true,
 	})
 	users := mvc.New(app.Party("/api/user", crs).AllowMethods())
@@ -121,7 +121,7 @@ func myMiddleware(ctx iris.Context) {
 	//ctx.Recorder().ResetHeaders()
 	//ctx.Header("Access-Control-Allow-Origin", "*")
 	//ctx.Header("Access-Control-Allow-Headers", "content-type")
-	
+	ctx.Header("Access-Control-Allow-Origin", "*")
 	fmt.Println("Method", ctx.Request().Method)
 	if ctx.Request().Method == "OPTIONS" {
 		fmt.Println("test for core")
